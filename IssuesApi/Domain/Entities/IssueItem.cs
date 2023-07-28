7dd9607d2012db05e13a88132ec9ebb40a0353cd
gabriel-panz <gabriel.panzenhagen@gmail.com>
2023-07-28 19:18:10 -0300

    added dockerfile

--- IssuesApi/Domain/Entities/IssueItem.cs
+++ IssuesApi/Domain/Entities/IssueItem.cs
@@ -8,7 +8,7 @@ public class IssueItem : BaseEntity
     public IssueStatus Status { get; set; }
     public string? Description { get; set; }
 
-    public Project Project { get; set; }
+    public Project? Project { get; set; }
     public long ProjectId { get; set; }
 }
 
---
+++ IssuesApi/Utils/DatabaseErrorHandling.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IssuesApi.Utils;
+
+public static class DatabaseErrorHandling
+{
+    public static string ConvertSqlite(string message)
+    {
+        ReadOnlySpan<string> values = message.Split("'");
+
+        if (!values[0].Contains("Duplicate entry"))
+            return message;
+
+        var value = values[1];
+        var constraint = values[3];
+
+        var property = constraint.Split("_").Last();
+
+        return property switch
+        {
+            "CardNumber" => $"442-{property}-{value}",
+            "Plate" => $"443-{property}-{value}",
+            _ => ""
+        };
+    }
+}
